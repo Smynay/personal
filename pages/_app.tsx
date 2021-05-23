44a71ac42a20ea9c9c2ae529deceb1438fda27b0
Nikita Schastnyy <smynay@yandex.ru>
2021-05-23 15:16:31 +0300

    Add app constructor

---
+++ pages/_app.tsx
@@ -0,0 +1,27 @@
+import { APP_GLOBAL_TITLE } from '@Utils/constants';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ReactElement, useEffect } from 'react';
+
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>{APP_GLOBAL_TITLE}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
