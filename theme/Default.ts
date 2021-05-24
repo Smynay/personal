6531bbcc38b3540a412069a764a96fbf01b211d5
Nikita Schastnyy <smynay@yandex.ru>
2021-05-24 11:49:48 +0300

    Add default theme colors

--- theme/Default.ts
+++ theme/Default.ts
@@ -1,5 +1,30 @@
 import { createMuiTheme } from '@material-ui/core';
 
-export enum Colors {}
+export enum Colors {
+  PRIMARY = '#303f9f',
+  SECONDARY = '#ef6461',
+  ACCENT = '#8c9eff',
+  TEXT = '#e8eaf6'
+}
 
-export const defaultTheme = createMuiTheme({});
+export const defaultTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      light: '#666ad1',
+      main: '#303f9f',
+      dark: '#001970',
+      contrastText: '#f7f7ff'
+    },
+    secondary: {
+      light: '#c0cfff',
+      main: '#8c9eff',
+      dark: '#5870cb',
+      contrastText: '#f7f7ff'
+    },
+    background: {
+      default: '#303f9f',
+      paper: '#e8eaf6'
+    }
+  }
+});
