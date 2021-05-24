56e74ca7a698779daf0c513e3d13b0ffc89f365f
Nikita Schastnyy <smynay@yandex.ru>
2021-05-24 11:49:10 +0300

    Add contacts constant

---
+++ utils/contacts.ts
@@ -0,0 +1,12 @@
+export const CONTACTS = [
+  {
+    icon: 'phone_in_call',
+    title: '8 (952) 219-00-10',
+    href: 'tel:8952190010'
+  },
+  {
+    icon: 'email',
+    title: 'smynay@yandex.ru',
+    href: 'mailto:smynay@yandex.ru'
+  }
+];
