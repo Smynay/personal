929b54d6af0f51eb36faca94121b32bf79b1acdb
Nikita Schastnyy <smynay@yandex.ru>
2021-05-24 11:52:12 +0300

    Customize header component

--- components/blocks/Header.tsx
+++ components/blocks/Header.tsx
@@ -1,18 +1,87 @@
+import { Box, Icon } from '@material-ui/core';
+import { Link as MuiLink } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+import { CONTACTS } from '@Utils/contacts';
+import Image from 'next/image';
 import Link from 'next/link';
-import React, { ReactElement } from 'react';
+import React, { EventHandler, ReactElement, SyntheticEvent } from 'react';
 
-const Header = (): ReactElement => (
-  <header>
-    <nav>
-      <Link href="/">
-        <a>Home</a>
-      </Link>{' '}
-      |{' '}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </nav>
-  </header>
-);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    border: 'none'
+  },
+  menu: {
+    display: 'none',
+    gap: theme.spacing(2),
+    [theme.breakpoints.up('md')]: {
+      display: 'flex'
+    }
+  },
+  menuItem: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: theme.spacing(1)
+  },
+  menuButton: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
+    }
+  }
+}));
+
+interface HeaderProps {
+  onMenuClick: EventHandler<SyntheticEvent>;
+}
+
+const Header = ({ onMenuClick }: HeaderProps): ReactElement => {
+  const classes = useStyles();
+
+  return (
+    <AppBar className={classes.root} position="sticky" color="transparent" variant="outlined">
+      <Toolbar>
+        <Box flexGrow={1}>
+          <Link href="/">
+            <MuiLink color="inherit" underline="none">
+              <Box display="flex" alignItems="center">
+                <Box mr={2}>
+                  <Image src="/assets/img/logo.svg" alt="" height="48" width="48" />
+                </Box>
+
+                <Typography variant="h6">Smynay.Space</Typography>
+              </Box>
+            </MuiLink>
+          </Link>
+        </Box>
+
+        <Box className={classes.menu} display="flex" alignItems="center">
+          {CONTACTS.map((item) => (
+            <Box key={`ct-${item.icon}`} className={classes.menuItem}>
+              <Icon>{item.icon}</Icon>
+              {item.href ? (
+                <MuiLink href={item.href} color="inherit" underline="none" variant="subtitle1">
+                  {item.title}
+                </MuiLink>
+              ) : (
+                <Typography variant="subtitle1">{item.title}</Typography>
+              )}
+            </Box>
+          ))}
+        </Box>
+        <IconButton
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+          onClick={onMenuClick}>
+          <MenuIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
